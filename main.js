bd5be13f20d07dcc17e96f8e1c0816c2efd7db38
Walter <walterrubicondo@gmail.com>
2021-03-16 18:09:40 +0100

    Esercizio completo

--- main.js
+++ main.js
@@ -41,40 +41,40 @@ $(document).ready( function () {
 
     // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-  var nome = prompt("inserisci il tuo nome");
-  var cognome = prompt("inserisci il tuo cognome");
-  var eta = prompt("inserisci la tua eta");
-
-  var studente =
-    {
-      nome: nome,
-      cognome: cognome,
-      eta: eta,
-    }
-    ;
-
-  studenti.push(studente);
-  console.log(studenti);
-  for (var key in studenti) {
-    $("#elenco").append(studenti[key].nome + " " + studenti[key].cognome + " " + studenti[key].eta + "<br>") }
+  // var nome = prompt("inserisci il tuo nome");
+  // var cognome = prompt("inserisci il tuo cognome");
+  // var eta = prompt("inserisci la tua eta");
+  //
+  // var studente =
+  //   {
+  //     nome: nome,
+  //     cognome: cognome,
+  //     eta: eta,
+  //   };
+  //
+  // studenti.push(studente);
+  // console.log(studenti);
+  // for (var key in studenti) {
+  //   $("#elenco").append(studenti[key].nome + " " + studenti[key].cognome + " " + studenti[key].eta + "<br>") }
 
 
 // Variante Input
 
-    // $("#add").click(function () {
-    //   var nome = $("#nome").val();
-    //   var cognome = $("#cognome").val();
-    //   var eta = $("#eta").val();
-    //
-    //   var studente = {
-    //     nome: nome,
-    //     cognome: cognome,
-    //     eta: eta,
-    //   }
-    //   studenti.push(studente)
-    //   for (var key in studenti) {
-    //     $("#elenco").append(studenti[key].nome + " " + studenti[key].cognome + " " + studenti[key].eta + "<br>")
-    //   }
-    // })
+    $("#add").click(function () {
+      var nome = $("#nome").val();
+      var cognome = $("#cognome").val();
+      var eta = $("#eta").val();
+
+      var studente = {
+        nome: nome,
+        cognome: cognome,
+        eta: eta,
+      }
+      studenti.push(studente)
+      $("#elenco").html("");
+      for (var key in studenti) {
+        $("#elenco").append(studenti[key].nome + " " + studenti[key].cognome + " " + studenti[key].eta + "<br>")
+      }
+    })
 
 })
